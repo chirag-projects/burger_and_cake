dd9b90405ae091059d8f809f5b4ae78511ebecac
chirag-projects <chirag.gulati258@gmail.com>
2026-05-09 16:02:33 +0530

    Added History Dashboard And Animated Graph

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,10 +3,10 @@ import { GoogleOAuthProvider } from "@react-oauth/google";
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-// 96557147819-20rrnei10kgrc54jie7h8s5c6tiviqh6.apps.googleusercontent.com
+// Add your client id at given place in the code below
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <GoogleOAuthProvider clientId="96557147819-20rrnei10kgrc54jie7h8s5c6tiviqh6.apps.googleusercontent.com">
+  <GoogleOAuthProvider clientId="add your client id here">
     <App />
   </GoogleOAuthProvider>
 );
